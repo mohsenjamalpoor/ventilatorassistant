ce8537758d5973002257c1d79cdf7a473b6957f9
m.jamalpour <mohsenjamal1023@gmail.com>
2026-08-03 01:08:06 +0330

    add

--- src/app/components/template/homePage.js
+++ src/app/components/template/homePage.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function HomePage() {
-  return <div>H</div>;
+  return (
+    <div className="min-h-screen bg-linear-to-br from-blue-50 to-cyan-100 py-8 px-4">
+      <div className="max-w-md mx-auto bg-white rounded-2xl shadow-2xl overflow-hidden">
+        {/* هدر */}
+        <div className="bg-linear-to-r from-blue-600 to-cyan-600 p-6 text-white text-center">
+          <h1 className="text-2xl font-bold mb-2"> دستیار ونتیلاتور</h1>
+          <p className="text-blue-100">لطفا اطلاعات بیمار را وارد کنید</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
