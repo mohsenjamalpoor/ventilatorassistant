93d23849da68cfbf82fd7c3aee32e9c65109fc67
m.jamalpour <mohsenjamal1023@gmail.com>
2026-08-03 01:26:26 +0330

    add

--- src/app/components/template/homePage.js
+++ src/app/components/template/homePage.js
@@ -1,6 +1,10 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 
 function HomePage() {
+  const [weight, setWeight] = useState("");
+  const [age, setAge] = useState("");
   return (
     <div className="min-h-screen bg-linear-to-br from-blue-50 to-cyan-100 py-8 px-4">
       <div className="max-w-md mx-auto bg-white rounded-2xl shadow-2xl overflow-hidden">
@@ -8,6 +12,32 @@ function HomePage() {
         <div className="bg-linear-to-r from-blue-600 to-cyan-600 p-6 text-white text-center">
           <h1 className="text-2xl font-bold mb-2"> دستیار ونتیلاتور</h1>
           <p className="text-blue-100">لطفا اطلاعات بیمار را وارد کنید</p>
+        </div>
+        <div className="p-6">
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="weight"
+            >
+              وزن بیمار (کیلوگرم)
+            </label>
+            <input
+              id="weight"
+              type="number"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              سن بیمار
+            </label>
+            <input
+              type="number"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
         </div>
       </div>
     </div>
