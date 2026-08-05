e3684a5fdf8c15cbb23987eeb5043aed7981bc27
m.jamalpour <mohsenjamal1023@gmail.com>
2026-08-05 12:03:19 +0330

    add

--- src/components/template/homePage.js
+++ src/components/template/homePage.js
@@ -331,20 +331,9 @@ function HomePage() {
       return;
     }
     if (Number(age) <= 0) {
-      toast.error("سن وارد شده معتبر نیست.");
+      toast.error("شن وارد شده معتبر نیست.");
       return;
     }
-
-    const params = new URLSearchParams({
-      weight,
-      age,
-      lungInvolvement,
-      ...(normalLungCondition && { normalLungCondition }),
-      ...(obstructiveDisease && { obstructiveDisease }),
-      ...(restrictiveDisease && { restrictiveDisease }),
-    });
-
-    router.push(`/ventilatortraining/setup?${params.toString()}`);
   };
 
   const ageNumber = Number(age);
