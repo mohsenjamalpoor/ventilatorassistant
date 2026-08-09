de1a238f62c3d44c98cd3b16d352dd77e4dc27e8
m.jamalpour <mohsenjamal1023@gmail.com>
2026-08-09 13:29:31 +0330

    add

--- src/components/module/RespiratoryAcidosisModal.js
+++ src/components/module/RespiratoryAcidosisModal.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function RespiratoryAcidosisModal() {
-  return <div>RespiratoryAcidosisModal</div>;
+  return (
+    <div className="w-[92vw] max-w-2xl max-h-[85vh] bg-white rounded-2xl shadow-2xl flex flex-col">
+      RespiratoryAcidosisModal
+    </div>
+  );
 }
 
 export default RespiratoryAcidosisModal;
